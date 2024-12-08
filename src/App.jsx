10b078b3942f84e8b5cff6d2c3108db08451bade
Muhammad Tayyab <tayyabxtrem@gmail.com>
2024-12-08 19:33:37 +0500

    website completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar'
 import Features from './components/Features'
 import Story from "./components/Story";
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
       <Features/>
       <Story/>
       <Contact />
-      
+      <Footer/>
     </main>
   )
 }
